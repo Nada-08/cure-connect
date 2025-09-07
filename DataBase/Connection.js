df9781a9b5ad3010ebd6f3c92a12a9685e69611a
Mennahassann <147339385+Mennahassann@users.noreply.github.com>
2025-09-08 01:49:50 +0300

    Delete DataBase directory

--- DataBase/Connection.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-const connection = ()=>{
-    mongoose.connect('mongodb://localhost:27017/Cure-Connect').then(()=>{
-        console.log('Database connected');
-    })
-}
-
-module.exports = connection
